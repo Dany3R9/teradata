2ee2999dc5d40b6586c83ebe5e0c2a542672ab65
Daniele <dany3r9@gmail.com>
2026-01-09 11:45:15 +0100

    Update systemCPU.sql
    formatted sql

--- systemCPU.sql
+++ systemCPU.sql
@@ -4,9 +4,10 @@ System CPU busy/available - 2016 march 01
 2018 10 31 -- updated with calculated column #Percent_busy
 */
 LOCK   ROW FOR access
-SELECT _DDate , SUM ( CPU_USR + CPU_SYS ) AS CPU_Busy , SUM ( CPU_WIO + CPU_IDLE ) AS CPU_IDLE ,
-                        SUM ( CPU_USR + CPU_SYS + CPU_WIO + CPU_IDLE ) AS CPU_available ,
-                        CAST( CPU_Busy / CPU_available  AS DEC ( 15 , 3 )  ) (NAMED #Percent_busy )
+SELECT _DDate , SUM ( CPU_USR + CPU_SYS ) AS CPU_Busy ,
+                SUM ( CPU_WIO + CPU_IDLE ) AS CPU_IDLE ,
+                SUM ( CPU_USR + CPU_SYS + CPU_WIO + CPU_IDLE ) AS CPU_available ,
+                CAST( CPU_Busy / CPU_available  AS DEC ( 15 , 3 )  ) (NAMED #Percent_busy )
 FROM   (
 SELECT thedate AS _DDate , nodeid AS Nodo , CAST( CAST( CAST( TheTime AS FORMAT '99:99:99.99'  ) AS CHAR ( 11 )  ) AS TIME( 6 )  ) AS time_of_day ,
                         SUM ( CPUUExec ) / 100 AS CPU_USR , SUM ( CPUUServ ) / 100 AS CPU_SYS ,
